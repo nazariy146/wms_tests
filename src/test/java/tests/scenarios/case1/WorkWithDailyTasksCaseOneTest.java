5c02ada043a30110e20bc84040988cb75ac006c4
OlegYeletskykh <Hawk304641994>
2021-12-07 16:26:20 +0200

    EleoRelocation1CCase2master2.1

--- src/test/java/tests/scenarios/case1/WorkWithDailyTasksCaseOneTest.java
+++ src/test/java/tests/scenarios/case1/WorkWithDailyTasksCaseOneTest.java
@@ -26,7 +26,7 @@ public class WorkWithDailyTasksCaseOneTest extends BaseMobileClass {
     LoadingCardPage loadingCardPage = new LoadingCardPage();
     InventoryCardPage inventoryCardPage = new InventoryCardPage();
     RelocationCardPage relocationCardPage = new RelocationCardPage();
-//    RelocationTSDCardPage relocationTSDCardPage = new RelocationTSDCardPage();
+    RelocationTSDCardPage relocationTSDCardPage = new RelocationTSDCardPage();
     SettingsPage settingsPage = new SettingsPage();
 
     @Test
@@ -551,14 +551,13 @@ public class WorkWithDailyTasksCaseOneTest extends BaseMobileClass {
         relocationCardPage.verifyData("unitAmount", "1.0");
         relocationCardPage.verifyData("unitPackaging", "10");
         relocationCardPage.clickButton("commit");
-
     }
 
     @Test (priority = 6, dependsOnMethods = "processingRelocationTaskTest")
     public void processingRelocationTSDTaskTest() throws Exception {
         steps.loginAsAdmin();
 
-  /*      relocationTSDCardPage.andrDriver("rightMenu");
+        relocationTSDCardPage.andrDriver("rightMenu");
         relocationTSDCardPage.clickButton("rightMenu");
         relocationTSDCardPage.clickButton("relocation");
     //Стол1
@@ -669,7 +668,7 @@ public class WorkWithDailyTasksCaseOneTest extends BaseMobileClass {
         relocationTSDCardPage.inputData("destination", "A.1.1.3.10");
         relocationTSDCardPage.verifyData("productInfo", "A.1.1.2.10 ➡ A.1.1.3.10 00010 Стол10 Quantity 10");
         relocationTSDCardPage.clickButton("moving");
-  */  }
+    }
 
     @Test (priority = 7, dependsOnMethods = "processingAccommodationTaskTest")
     public void processingSelectionTaskTest() throws Exception {
